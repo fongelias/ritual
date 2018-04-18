0dd98f70c0ab6e7ced3a17ab338cb74b2d58059c
fongelias <fongelias@gmail.com>
2018-04-18 17:08:42 -0400

    added directory for lambda source

---
+++ src/lambdas/addUser.js
@@ -0,0 +1,8 @@
+exports.handler = async (event, context, callback) => {
+    // TODO implement
+    console.log(context);
+    //Change to make sure travis deploy is being tested properly
+    callback(null, {
+        body: 'Hello from Lambda!'
+    });
+};
