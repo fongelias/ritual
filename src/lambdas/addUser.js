5573ec69cff45d713f3942e4aa3d2d106baa9a97
fongelias <fongelias@gmail.com>
2018-04-18 21:16:39 -0400

    polyfill test

--- src/lambdas/addUser.js
+++ src/lambdas/addUser.js
@@ -1,10 +1,2 @@
 import 'babel-polyfill';
-
-exports.handler = async (event, context, callback) => {
-    // TODO implement
-    console.log(context);
-    //Change to make sure travis deploy is being tested properly
-    callback(null, {
-        body: 'Hello from Lambda!'
-    });
-};
+import './addUserSource';
