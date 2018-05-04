069cb46433b7eab29f5c009a0603bbac794c0f49
fongelias <fongelias@gmail.com>
2018-05-04 15:34:34 -0400

    added add user method and corrected indexing

--- src/lambdas/retrieveUser.js
+++ src/lambdas/retrieveUser.js
@@ -5,25 +5,26 @@ const AWS = require('aws-sdk');
 const config = {
 	USERTABLE: {
 		NAME: 'ritual-app-users',
-		INDEX: 'cognitoIdentityId',
+		PRIMARY_KEY: 'cognitoIdentityId',
 	}
 }
 
 exports.handler = async (event, context, callback) => {
     console.log(event);
-    console.log(context);
     console.log(event.requestContext.identity.cognitoIdentityId)
     //Change to make sure travis deploy is being tested properly
     const headers = {
     	"Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-            "Access-Control-Allow-Credentials" : true, // Required for cookies, authorization headers with HTTPS 
+        "Access-Control-Allow-Credentials" : true, // Required for cookies, authorization headers with HTTPS 
     }
 
-    findUserById(event.requestContext.identity.cognitoIdentityId).then((data) => {
+    retrieveUser(event.requestContext.identity.cognitoIdentityId).then((data) => {
     	callback(null, {
     		headers,
     		body: data,
     	})
+    }).catch((err) => {
+    	console.log(err);
     })
 
     /*callback(null, {
@@ -34,14 +35,19 @@ exports.handler = async (event, context, callback) => {
 
 
 
+function retrieveUser(id) {
+	findUserById(id).then(user => user).catch(err => addUser(id))
+}
+
+
+
 /*--DynamoDB functions--*/
 function findUserById(id) {
-	console.log("Querying " + config.USERTABLE.NAME + " for user with " + config.USERTABLE.INDEX + " " + id);
+	console.log("Querying " + config.USERTABLE.NAME + " for user with " + config.USERTABLE.PRIMARY_KEY + " " + id);
 	const docClient = new AWS.DynamoDB.DocumentClient();
 	const params = {
 		TableName: config.USERTABLE.NAME,
-		IndexName: config.USERTABLE.INDEX,
-		KeyConditionExpression: config.USERTABLE.INDEX + " = :cognitoId",
+		KeyConditionExpression: config.USERTABLE.PRIMARY_KEY + " = :cognitoId",
 		ExpressionAttributeValues: {
 			":cognitoId": id,
 		}
@@ -55,6 +61,31 @@ function findUserById(id) {
 				reject(errMessage);
 			} else {
 				console.log(data);
+				resolve(data);
+			}
+		})
+	})
+}
+
+function addUser(id) {
+	console.log("Adding new user to " + config.USERTABLE.NAME);
+
+	const docClient = new AWS.DynamoDB.DocumentClient();
+	const params = {
+		TableName: config.USERTABLE.NAME,
+		Item: {
+			'cognitoIdentityId': id,
+		}
+	}
+
+	return new Promise((resolve, reject) => {
+		docClient.put(params, (err, data) => {
+			if(err) {
+				const errMessage = "Unable to add user";
+				console.error(errMessage + " Error JSON:" + JSON.stringify(err));
+				reject(errMessage);
+			} else {
+				console.log("Added user:", JSON.stringify(data));
 				resolve(data);
 			}
 		})
